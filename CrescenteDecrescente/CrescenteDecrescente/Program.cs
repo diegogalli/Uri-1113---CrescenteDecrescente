c7b49545d779a1568731f723a379ac8b3be88a7a
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:45:56 -0300

    Uri 1113 Crescente e Decrescente

---
+++ CrescenteDecrescente/CrescenteDecrescente/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CrescenteDecrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            string[] dados = Console.ReadLine().Split(' ');
+            x = int.Parse(dados[0]);           
+            y = int.Parse(dados[1]);           
+
+            while ( x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                dados = Console.ReadLine().Split(' ');
+                x = int.Parse(dados[0]);
+                y = int.Parse(dados[1]);
+
+            }
+
+        }
+    }
+}
